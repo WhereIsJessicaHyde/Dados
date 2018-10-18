ebde05b44e3e487ccae20be4169656502211337a
anjenaex <anjenaex@gmail.com>
2018-10-18 17:18:58 +0200

    logica y hmtl en prototipo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+// voy a crear una clase constructora que genere un numero de forma aleatoria.
 
+class Dado {
+  constructor (numeroDeCaras, tiradaMinima) {
+    this.numeroDeCaras = numeroDeCaras;
+    this.tiradaMinima = tiradaMinima;
+    let resultado = 0;
+  }
+  numeroAleatorio () {
+    let resultado = Math.floor(Math.random() * (this.numeroDeCaras - this.tiradaMinima) + this.tiradaMinima);
+    return resultado;
+  }
+}
